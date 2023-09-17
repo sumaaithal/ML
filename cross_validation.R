c24123e359d450d461f2889a6d3cbd4f58b23075
Suma Aithal <38108871+sumaaithal@users.noreply.github.com>
2023-09-17 18:20:51 +0530

    Add files via upload
    cross val, grid Search

---
+++ cross_validation.R
@@ -0,0 +1,85 @@
+# import the data
+dataset = read.csv("Social_Network_Ads.csv")
+
+library(caTools)
+library(ggplot2)
+library(e1071)
+set.seed(42)
+
+
+#split the data
+split = sample.split(dataset$Purchased,SplitRatio = 0.75)
+train_split = subset(dataset,split==TRUE )
+test_split = subset(dataset,split==FALSE)
+
+#feature scaling
+train_split[-3] = scale(train_split[-3])
+test_split[-3] = scale(test_split[-3])
+
+## fitting log reg to training set
+classifier = svm(formula = Purchased ~., data=train_split, 
+                 type='C-classification',
+                 kernel='radial')
+
+y_pred = predict(classifier,newdata = test_split[-3])
+
+#built confusion matrix
+cm = table(test_split[,3],y_pred)
+
+cm
+
+##applying k-fold cross validation
+#install.packages('caret')
+library(caret)
+
+folds = createFolds(train_split$Purchased,k=10)
+
+cv = lapply(folds,function(x){
+  training_fold = train_split[-x,]
+  test_fold = train_split[x,]
+  classifier = svm(formula = Purchased ~., data=training_fold, 
+                   type='C-classification',
+                   kernel='radial')
+  y_pred = predict(classifier,newdata = test_fold[-3])
+  cm = table(test_fold[,3],y_pred)
+  
+  accuracy = (cm[1,1]+cm[2,2])/(cm[1,1]+cm[2,2]+cm[1,2]+cm[2,1])
+  return(accuracy)
+})
+
+mean(as.numeric(cv))
+
+#visualizing the data
+
+library(ElemStatLearn)
+set = train_split
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+
+plot(set[, -3],
+     main = 'SVm (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'dodgerblue', 'salmon'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'dodgerblue3', 'salmon3'))
+
+# Visualising the Test set results
+library(ElemStatLearn)
+set = test_split
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+
+plot(set[, -3],
+     main = 'SVM (Test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'dodgerblue', 'salmon'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'dodgerblue3', 'salmon3'))
